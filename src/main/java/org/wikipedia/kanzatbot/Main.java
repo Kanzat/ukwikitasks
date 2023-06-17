627803feea2ccc1a6ad57ee5f4e493c407dc89c2
Kanzat <kanzatbot@gmail.com>
2023-06-17 15:35:15 +0300

    Isolate operations one from another

--- src/main/java/org/wikipedia/kanzatbot/Main.java
+++ src/main/java/org/wikipedia/kanzatbot/Main.java
@@ -1,5 +1,6 @@
 package org.wikipedia.kanzatbot;
 
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.HttpUrl;
 import org.fastily.jwiki.core.Wiki;
 import org.wikipedia.kanzatbot.auth.Auth;
@@ -8,6 +9,7 @@ import org.wikipedia.kanzatbot.deletedrestored.DeletedPagesRestored;
 import org.wikipedia.kanzatbot.potd.CreateCandidateImagesPage;
 import org.wikipedia.kanzatbot.potd.ImportImages;
 
+@Slf4j
 public class Main {
 
     public static Wiki ukWiki;
@@ -21,14 +23,23 @@ public class Main {
         ukWiki = new Wiki.Builder().withApiEndpoint(HttpUrl.parse("https://uk.wikipedia.org/w/api.php")).build();
         commonsWiki = new Wiki.Builder().withApiEndpoint(HttpUrl.parse("https://commons.wikimedia.org/w/api.php")).build();
         Auth.auth(ukWiki);
-        CreateCandidateImagesPage.create(ukWiki, "Вікіпедія:Вікі любить пам'ятки/Переможці", ukWiki, "Вікіпедія:Проєкт:Вибране зображення/Кандидати/Вікі любить пам'ятки",
-                "Ця сторінка містить [[Вікіпедія:Вікі любить пам'ятки/Переможці|переможців конкурсу Вікі любить пам'ятки]], які ще жоден раз не ставали вибраним зображенням на головній сторінці");
-        CreateCandidateImagesPage.create(ukWiki, "Вікіпедія:Вікі любить Землю/Переможці", ukWiki, "Вікіпедія:Проєкт:Вибране зображення/Кандидати/Вікі любить Землю",
-                "Ця сторінка містить [[Вікіпедія:Вікі любить Землю/Переможці|переможців конкурсу Вікі любить Землю]], які ще жоден раз не ставали вибраним зображенням на головній сторінці");
-        ImportImages.startImport();
-        // CreateCandidatePatrolsPage.create();
-        CopyvioDetector.runInNewPages(ukWiki);
-        DeletedPagesRestored.find(ukWiki);
+        runIsolated(() -> CreateCandidateImagesPage.create(ukWiki, "Вікіпедія:Вікі любить пам'ятки/Переможці", ukWiki, "Вікіпедія:Проєкт:Вибране зображення/Кандидати/Вікі любить пам'ятки",
+                "Ця сторінка містить [[Вікіпедія:Вікі любить пам'ятки/Переможці|переможців конкурсу Вікі любить пам'ятки]], які ще жоден раз не ставали вибраним зображенням на головній сторінці"));
+        runIsolated(() -> CreateCandidateImagesPage.create(ukWiki, "Вікіпедія:Вікі любить Землю/Переможці", ukWiki, "Вікіпедія:Проєкт:Вибране зображення/Кандидати/Вікі любить Землю",
+                "Ця сторінка містить [[Вікіпедія:Вікі любить Землю/Переможці|переможців конкурсу Вікі любить Землю]], які ще жоден раз не ставали вибраним зображенням на головній сторінці"));
+        runIsolated(() -> ImportImages.startImport());
+        // runIsolated(() -> CreateCandidatePatrolsPage.create());
+        runIsolated(() -> CopyvioDetector.runInNewPages(ukWiki));
+        runIsolated(() -> DeletedPagesRestored.find(ukWiki));
     }
+
+    private static void runIsolated(Runnable runnable) {
+        try {
+            runnable.run();
+        } catch (Exception e) {
+            log.error("Operation failed", e);
+        }
+    }
+
 
 }
